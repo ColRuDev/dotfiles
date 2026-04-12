304c4249c46d443860e98378000c0f6f0606ab91
ColRuDev <47890683+ColRuDev@users.noreply.github.com>
2026-04-12 18:09:08 -0500

    Add plugins to nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -3,17 +3,17 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Check if the lazy.nvim plugin is not already installed
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -25,34 +25,43 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
--- import your plugins
-  spec = {
-    -- Add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- theme with transparency support
-    { 'projekt0n/github-nvim-theme', name = 'github-theme', priority = 1000 },
+	-- import your plugins
+	spec = {
+		-- Add LazyVim and import its plugins
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		-- theme with transparency support
+		{ "projekt0n/github-nvim-theme", name = "github-theme", priority = 1000 },
 
-    -- Search files
-    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
+		-- Search files
+		{ import = "lazyvim.plugins.extras.editor.snacks_picker" },
 
-    -- Formatting plugins
-    { import = "lazyvim.plugins.extras.lang.typescript.biome" },
+		-- Formatting plugins
+		{ import = "lazyvim.plugins.extras.lang.typescript.biome" },
 
-    -- Linting plugins
-    { import = "lazyvim.plugins.extras.linting.eslint" },
+		-- Linting plugins
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
 
-    -- Coding plugins
-    { import = "lazyvim.plugins.extras.coding.mini-surround" }, -- Surround text with brackets/quotes
-    { import = "lazyvim.plugins.extras.editor.mini-diff" }, -- Git blame
-    { import = "lazyvim.plugins.extras.coding.blink" }, -- auto-completion
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" }, -- highlights text patterns
-    -- AI plugins
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-  },
+		-- Coding plugins
+		{ import = "lazyvim.plugins.extras.coding.mini-surround" }, -- Surround text with brackets/quotes
+		{ import = "lazyvim.plugins.extras.editor.mini-diff" }, -- Git blame
+		{ import = "lazyvim.plugins.extras.coding.blink" }, -- auto-completion
+		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" }, -- highlights text patterns
 
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+		-- Syntax plugins
+		{ import = "lazyvim.plugins.extras.lang.astro" },
+		{ import = "lazyvim.plugins.extras.lang.docker" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.markdown" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		-- AI plugins
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
+	},
+
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
